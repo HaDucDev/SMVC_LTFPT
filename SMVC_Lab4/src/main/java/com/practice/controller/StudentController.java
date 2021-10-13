446ed62adeb8451a80c4907cf20e3d8ef5db6460
HaDucDev <haductrung392000@gmail.com>
2021-10-13 09:47:11 +0700

    Done_L4_B2

--- SMVC_Lab4/src/main/java/com/practice/controller/StudentController.java
+++ SMVC_Lab4/src/main/java/com/practice/controller/StudentController.java
@@ -1,11 +1,17 @@
 package com.practice.controller;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -25,5 +31,24 @@ public class StudentController {
 		session.setAttribute("salary", 1000);
 		request.setAttribute("photo", "images/sv.png");
 		return "student/index";
+	}
+	
+	
+	@RequestMapping("index2")
+	public String index2(ModelMap model) {
+	Student sv1 = new Student("Bạch Liên Nhi", 8.5, "Ứng dụng phần mềm");
+	Student sv2 = new Student("THHD", 9.5, "Thiết kế trang web");
+	Student sv3 = new Student("Nguyễn Huyền Linh", 8.5, "Thiết kế trang web");
+	List<Student> list = new ArrayList<>();
+	list.add(sv2);
+	list.add(sv3);
+	Map<String, Student> map = new HashMap<>();
+	map.put("BLN", sv2);
+	map.put("NHL", sv3);
+	
+	model.addAttribute("bean", sv1);
+	model.addAttribute("list", list);
+	model.addAttribute("map", map);
+	return "student/index2";
 	}
 }
